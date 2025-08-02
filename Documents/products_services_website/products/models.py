6b6ffd25453c773ba6c53dc0b58f873d6e46b09a
Michael-oss138 <139558168+Michael-oss138@users.noreply.github.com>
2025-08-02 23:43:15 +0100

    worked on the products model

--- Documents/products_services_website/products/models.py
+++ Documents/products_services_website/products/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils.text import slugify
+# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Product_Categories'
+        ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete = models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name= 'products'
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    uploaded_at = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
+    
+    
