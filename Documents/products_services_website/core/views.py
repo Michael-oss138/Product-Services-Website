a2c04a155e8f2c0aa4ff152daf8fbb26636f1afd
Michael-oss138 <139558168+Michael-oss138@users.noreply.github.com>
2025-08-02 23:49:45 +0100

    trying to work on the registration view here

--- Documents/products_services_website/core/views.py
+++ Documents/products_services_website/core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 
-# Create your views here.
+ Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def products(request):
+    return render(request, 'core/products.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+#
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password = request.POST['password2']
+
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                message.info(request, 'Username already used')
+                return redirect('register')
+
+            elif User.objects.filter(email=email).exists():
+                message.info(request, 'Email already used')
+                return redirect('register')
+
+            else:
+                User.Objects.create(usernamae=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+
+        else:
+            message.info(request, 'Password not the same')
+            return redirect('register')
+
+    else:
+        return render(request, 'core/register.html',{'title': 'Register To Get Started'})
